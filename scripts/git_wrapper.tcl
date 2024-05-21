bdd6e672f3829760db947b4121bb5b08e50ded3a
Vishal Chani <vishal.chani@silicon-austria.com>
2024-05-21 12:39:10 +0200

    Removed Argument. Handler is called for all projects.

--- scripts/git_wrapper.tcl
+++ scripts/git_wrapper.tcl
@@ -84,65 +84,65 @@ namespace eval ::git_wrapper {
         set proj_file [current_project].tcl
         puts "$proj_dir"
         write_project_tcl_git -no_copy_sources -force $proj_file
-        if { ("-mig"  in $args) } {
-            set PathList {}
-            if {[catch {open  $proj_file r} file]} {
+        #if { ("-mig"  in $args) } {
+        set PathList {}
+        if {[catch {open  $proj_file r} file]} {
+            puts "Error: Unable to open file $proj_file"
+        } else {
+            while {[gets $file line] != -1} {
+                if {[string match "*\[file normalize *\[\"*\]*.prj\"*\]*" $line]} {
+
+                    set start [string first "\"" $line]
+                    set end [string last "\"" $line]
+                    set path [string range $line [expr {$start + 1}] [expr {$end - 1}]]
+                    
+
+                    if {[string match "*{origin_dir}/vivado_project/*" $path]} {
+                        set pathTemp [string map [list "\${origin_dir}/" ""] $path]
+                        set destination_path "$proj_dir/prj_files/$pathTemp"
+                        set directory [file dirname $pathTemp]
+                        set destDirectory "$proj_dir/prj_files/$directory"
+                        set newInFilePath "\${origin_dir}/prj_files/$pathTemp"
+                        lappend PathList [list $path $newInFilePath]
+
+                        if {![file isdirectory $destDirectory]} {
+                            puts "\[INFO\] prj_files Directory Created"
+                            file mkdir $destDirectory
+                        }
+
+                        file copy -force $pathTemp $destination_path
+                    }
+                }
+            }
+            close $file
+        }
+
+
+        if {[file exists $proj_file]} {
+            # Open the input file for reading
+            if {[catch {open  $proj_file r} original_scriptfile]} {
                 puts "Error: Unable to open file $proj_file"
             } else {
-                while {[gets $file line] != -1} {
-                    if {[string match "*\[file normalize *\[\"*\]*.prj\"*\]*" $line]} {
-
-                        set start [string first "\"" $line]
-                        set end [string last "\"" $line]
-                        set path [string range $line [expr {$start + 1}] [expr {$end - 1}]]
-                        
-
-                        if {[string match "*{origin_dir}/*" $path]} {
-                            set pathTemp [string map [list "\${origin_dir}/" ""] $path]
-                            set destination_path "$proj_dir/prj_files/$pathTemp"
-                            set directory [file dirname $pathTemp]
-                            set destDirectory "$proj_dir/prj_files/$directory"
-                            set newInFilePath "\${origin_dir}/prj_files/$pathTemp"
-                            lappend PathList [list $path $newInFilePath]
-
-                            if {![file isdirectory $destDirectory]} {
-                                puts "\[INFO\] prj_files Directory Created"
-                                file mkdir $destDirectory
-                            }
-
-                            file copy -force $pathTemp $destination_path
-                        }
-                    }
+                set script_content [read $original_scriptfile]
+                close $original_scriptfile
+                foreach {inputPath} $PathList {
+                    set oldPath [lindex $inputPath 0]
+                    set newPath [lindex $inputPath 1]
+                    set script_content [string map [list $oldPath $newPath] $script_content]
+                    set oldPath [string map {"{" "" "}" ""} $oldPath]
+                    set newPath [string map {"{" "" "}" ""} $newPath]
+                    set script_content [string map [list $oldPath $newPath] $script_content]
                 }
-                close $file
-            }
+        
+                set modified_script_file $proj_file
+                set modified_script [open $modified_script_file w]
+                # Write the modified content to the modified script file
+                puts -nonewline $modified_script $script_content
 
-
-            if {[file exists $proj_file]} {
-                # Open the input file for reading
-                if {[catch {open  $proj_file r} original_scriptfile]} {
-                    puts "Error: Unable to open file $proj_file"
-                } else {
-                    set script_content [read $original_scriptfile]
-                    close $original_scriptfile
-                    foreach {inputPath} $PathList {
-                        set oldPath [lindex $inputPath 0]
-                        set newPath [lindex $inputPath 1]
-                        set script_content [string map [list $oldPath $newPath] $script_content]
-                        set oldPath [string map {"{" "" "}" ""} $oldPath]
-                        set newPath [string map {"{" "" "}" ""} $newPath]
-                        set script_content [string map [list $oldPath $newPath] $script_content]
-                    }
-            
-                    set modified_script_file $proj_file
-                    set modified_script [open $modified_script_file w]
-                    # Write the modified content to the modified script file
-                    puts -nonewline $modified_script $script_content
-
-                    # Close the modified script file
-                    close $modified_script
-                }
+                # Close the modified script file
+                close $modified_script
             }
         }
+       # }
     }
 }
